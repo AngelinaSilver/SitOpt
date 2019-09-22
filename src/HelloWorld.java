4ab9be098aa1b829339f8df874e80eb3b8eec06b
ametodi <ametodi@PC-AMETODI-T441.global.cadence.com>
2019-09-22 13:30:13 +0300

    Testing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -26,7 +26,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
        // The model is the main component of Choco Solver
-       Model model = new Model("Choco Solver Hello World");
+       Model model = new Model("Choco Solver Hello World xxx");
        // Integer variables
        IntVar a = model.intVar("a", new int[]{4, 6, 8}); // takes value in { 4, 6, 8 }
        IntVar b = model.intVar("b", 0, 2); // takes value in [0, 2]
