c9885a07c91f674fc53129b2b56116302090519c
rachela <rachela@cadence.com>
2019-09-22 20:41:33 +0300

    Merge branch 'master' of https://github.com/AngelinaSilver/SitOpt
    # Conflicts:
    #	src/ImagePanel.java
    #	src/Seating.java

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 
 public class ImagePanel extends JPanel {
-    private String imagePath = "C:\\Hackaton\\map.gif";
+    private String imagePath = "C:\\Hackathon\\map.gif";
 
     private BufferedImage myPicture=null;
     private ImageIcon icon= null;
